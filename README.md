1e09b6ccd40f24921dd2d370333bba47bae4c6c7
zxfjd3g <258147149@qq.com>
2020-01-03 08:14:17 +0800

    note

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
-## 系列问题
-    1). 2种定义组件方式的比较?
-    2). 区别webpack和webpack-cli?
-    3). webpack的几个基本配置项?
-    4). 区别webpack的开发环境与生产环境?
-    5). 说几个webpack中常用的工具包?
-    6). 区别babel的plugin与preset?
+
 
--- webpack.config.js
+++ webpack.config.js
@@ -100,7 +100,7 @@ module.exports = {
 
 /* 
 理解babel的plugin(插件)与preset(预设)
-    babel本身不编译ES6的语法
+    babel本身不能编译ES6的语法
     babel需要基于它的plugin来做ES语法的编译
     每个语法都一个对应的babel plugin包来编译对应的语法
     一个babel preset包是包含多个常用的babel plugin的集合包
